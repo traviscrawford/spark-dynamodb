30f8e6c708cebcd1d92f2b029ea22cff46a84aea
Travis Crawford <travis@medium.com>
2016-10-06 21:06:07 -0700

    Refactor some common bits into BaseScanner

--- src/main/scala/com/github/traviscrawford/spark/dynamodb/BaseScanner.scala
+++ src/main/scala/com/github/traviscrawford/spark/dynamodb/BaseScanner.scala
@@ -6,6 +6,9 @@ import com.amazonaws.regions.Regions
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
 import com.amazonaws.services.dynamodbv2.document.Table
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec
+import com.amazonaws.services.dynamodbv2.model.ReturnConsumedCapacity
+import org.apache.spark.sql.types.StructType
 import org.slf4j.LoggerFactory
 
 
@@ -32,4 +35,33 @@ private[dynamodb] trait BaseScanner {
     maybeEndpoint.foreach(amazonDynamoDBClient.setEndpoint) // for tests
     new DynamoDB(amazonDynamoDBClient).getTable(tableName)
   }
+
+  def getTable(config: ScanConfig): Table = {
+    getTable(
+      tableName = config.table,
+      config.maybeCredentials,
+      config.maybeRegion,
+      config.maybeEndpoint)
+  }
+
+  def getScanSpec(config: ScanConfig): ScanSpec = {
+    new ScanSpec()
+      .withMaxPageSize(config.pageSize)
+      .withReturnConsumedCapacity(ReturnConsumedCapacity.TOTAL)
+      .withTotalSegments(config.totalSegments)
+      .withSegment(config.segment)
+  }
 }
+
+private[dynamodb] case class ScanConfig(
+  table: String,
+  segment: Int,
+  totalSegments: Int,
+  pageSize: Int,
+  maybeSchema: Option[StructType] = None,
+  maybeRequiredColumns: Option[Array[String]] = None,
+  maybeRateLimit: Option[Int] = None,
+  maybeCredentials: Option[String] = None,
+  maybeRegion: Option[String] = None,
+  maybeEndpoint: Option[String] = None
+)
