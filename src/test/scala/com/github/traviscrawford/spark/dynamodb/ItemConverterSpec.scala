4ff66fddbeca23e361e32000bcb4b667632711fd
Eduardo Ramirez <eduardo@medium.com>
2019-01-10 16:31:48 -0800

    Ignore schema fields that are not present in the db item (#51)

--- src/test/scala/com/github/traviscrawford/spark/dynamodb/ItemConverterSpec.scala
+++ src/test/scala/com/github/traviscrawford/spark/dynamodb/ItemConverterSpec.scala
@@ -38,4 +38,35 @@ class ItemConverterSpec extends FlatSpec with Matchers {
     ItemConverter.toRow(item, schema) shouldBe
       Row("a", Seq("a"), 1, Seq(1), 2L, Seq(2L), 3.3f, Seq(3.3f), 4.4d, Seq(4.4d))
   }
+
+  it should "correctly transform an Item into a Row if item is missing a field" in {
+    val item = new Item()
+      .withList("testStringList", Seq("a").asJava)
+      .withInt("testInt", 1)
+      .withList("testIntList", Seq(1).asJava)
+      .withLong("testLong", 2L)
+      .withList("testLongList", Seq(2L).asJava)
+      .withFloat("testFloat", 3.3f)
+      .withList("testFloatList", Seq(3.3f).asJava)
+      .withDouble("testDouble", 4.4d)
+      .withList("testDoubleList", Seq(4.4d).asJava)
+
+    val schema = StructType(Seq(
+      StructField("testString", StringType),
+      StructField("testStringList", ArrayType(StringType)),
+      StructField("testInt", IntegerType),
+      StructField("testIntList", ArrayType(IntegerType)),
+      StructField("testLong", LongType),
+      StructField("testLongList", ArrayType(LongType)),
+      StructField("testFloat", FloatType),
+      StructField("testFloatList", ArrayType(FloatType)),
+      StructField("testDouble", DoubleType),
+      StructField("testDoubleList", ArrayType(DoubleType))
+    ))
+
+    // scalastyle:off null
+    ItemConverter.toRow(item, schema) shouldBe
+      Row(null, Seq("a"), 1, Seq(1), 2L, Seq(2L), 3.3f, Seq(3.3f), 4.4d, Seq(4.4d))
+    // scalastyle:on null
+  }
 }
